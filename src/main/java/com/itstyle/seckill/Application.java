3ebc17c532c839cb652e4f287f70c214e3049f0b
小柒2012 <345849402@qq.com>
2019-02-21 22:04:37 +0800

    更新 Application.java

--- src/main/java/com/itstyle/seckill/Application.java
+++ src/main/java/com/itstyle/seckill/Application.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 创建者 科帮网
  * 创建时间	2018年5月12日
  * API接口测试：http://localhost:8080/seckill/swagger-ui.html
+ * 跑之前 一定要看文库：https://gitee.com/52itstyle/spring-boot-seckill/wikis
  */
 @SpringBootApplication
 public class Application {
